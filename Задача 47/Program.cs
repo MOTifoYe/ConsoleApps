ce88f649e5d7447bc7afefe7a5a1ffb709bdfec6
Тимофей Перминов <goaway0137@outlook.com>
2021-12-27 01:42:40 +0700

    новые задачки

---
+++ Задача 47/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_47
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введи оценку: ");
+            string mark = Console.ReadLine();
+            switch (mark)
+            {
+                case "2": Console.WriteLine("Неудовлетворительно");
+                    break;
+                case "3": Console.WriteLine("Удовлетворительно");
+                    break;
+                case "4": Console.WriteLine("Хорошо");
+                    break;
+                case "5": Console.WriteLine("Отлично");
+                    break;
+                default: Console.WriteLine("Чет не правильно!!");
+                    break;
+            } 
+        }
+    }
+}
---
+++ Задача 48/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_48
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "";
+
+            Console.Write("Сколько тебе лет, определим тебя в отряд: ");
+            a = Console.ReadLine();
+            
+            switch (a)
+            {
+                case "6": Console.WriteLine("У тебя 5 отряд!");
+                    break;
+                case "7": Console.WriteLine("У тебя 5 отряд!");
+                    break;
+                case "8": Console.WriteLine("У тебя 4 отряд!");
+                    break;
+                case "9": Console.WriteLine("У тебя 4 отряд!");
+                    break;
+                case "10": Console.WriteLine("У тебя 3 отряд!");
+                    break;
+                case "11": Console.WriteLine("У тебя 3 отряд!");
+                    break;
+                case "12": Console.WriteLine("У тебя 2 отряд!");
+                    break;
+                case "13": Console.WriteLine("У тебя 2 отряд!");
+                    break;
+                case "14": Console.WriteLine("У тебя 1 отряд!");
+                    break;
+                case "15": Console.WriteLine("У тебя 1 отряд!");
+                    break;
+                default: Console.WriteLine("Ты не подходишь по возрасту");
+                    break;
+            }
+        }
+    }
+}
