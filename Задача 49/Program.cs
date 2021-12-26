4688c8f6e46ee30121d879fb76038401e8f35935
Тимофей Перминов <goaway0137@outlook.com>
2021-12-27 01:42:40 +0700

    Добавьте файлы проекта.

---
+++ Задача 49/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_49
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "";
+
+            Console.WriteLine("0 - выход");
+            while (true)
+            {
+            Console.Write("Введи букву: ");
+            a = Console.ReadLine();
+                if (a == "0")
+                {
+                    break;
+                }
+                switch (a)
+                {
+                    case "а":
+                        Console.WriteLine("гласный");
+                        break;
+                    case "б":
+                        Console.WriteLine("согласный,звонкий");
+                        break;
+                    case "в":
+                        Console.WriteLine("согласный,звонкий");
+                        break;
+                    case "г":
+                        Console.WriteLine("согласный,звонкий");
+                        break;
+                    case "д":
+                        Console.WriteLine("согласный,звонкий");
+                        break;
+                    case "е":
+                        Console.WriteLine("гласный");
+                        break;
+                    case "ё":
+                        Console.WriteLine("гласный");
+                        break;
+                    case "ж":
+                        Console.WriteLine("согласный,звонкий");
+                        break;
+                    case "з":
+                        Console.WriteLine("согласный,звонкий");
+                        break;
+                    case "и":
+                        Console.WriteLine("гласный");
+                        break;
+                    case "и́":
+                        Console.WriteLine("согласный,звонкий");
+                        break;
+                    case "к":
+                        Console.WriteLine("согласный,глухой");
+                        break;
+                    case "л":
+                        Console.WriteLine("согласный,звонкий");
+                        break;
+                    case "м":
+                        Console.WriteLine("согласный,звонкий");
+                        break;
+                    case "н":
+                        Console.WriteLine("согласный,звонкий");
+                        break;
+                    case "о":
+                        Console.WriteLine("гласный");
+                        break;
+                    case "п":
+                        Console.WriteLine("согласный,глухой");
+                        break;
+                    case "р":
+                        Console.WriteLine("согласный,звонкий");
+                        break;
+                    case "с":
+                        Console.WriteLine("согласный,глухой");
+                        break;
+                    case "т":
+                        Console.WriteLine("согласный,глухой");
+                        break;
+                    case "у":
+                        Console.WriteLine("гласный");
+                        break;
+                    case "ф":
+                        Console.WriteLine("согласный,глухой");
+                        break;
+                    case "х":
+                        Console.WriteLine("согласный,глухой");
+                        break;
+                    case "ц":
+                        Console.WriteLine("согласный,глухой");
+                        break;
+                    case "ч":
+                        Console.WriteLine("согласный,глухой");
+                        break;
+                    case "ш":
+                        Console.WriteLine("согласный,глухой");
+                        break;
+                    case "щ":
+                        Console.WriteLine("согласный,глухой");
+                        break;
+                    case "ы":
+                        Console.WriteLine("гласный");
+                        break;
+                    case "ъ":
+                        Console.WriteLine("-");
+                        break;
+                    case "ь":
+                        Console.WriteLine("-");
+                        break;
+                    case "э":
+                        Console.WriteLine("гласный");
+                        break;
+                    case "ю":
+                        Console.WriteLine("гласный");
+                        break;
+                    case "я":
+                        Console.WriteLine("гласный");
+                        break;
+
+                    default:
+                        Console.WriteLine("Ошибка");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Задача 50/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_50
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            string[] fraz1 = new string[] 
+            {
+                "Я калькулятор Женя,\nпосчитай во мне полностью",
+                "Приветствую, мой господин",
+                "Я заждалась тебя, братик",
+                "Ты вспомнил обо мне, проведем еще одну ночь вместе?",
+                "Спс за ночку, дорогая", 
+                "Потрогай еще циферки, мне это очень нравится",
+                "Без тебя так одиноко...",
+                "Яматекудасай",
+                "Я ваша горничная,господин",
+                "Не уходи от меня больше",
+                "А ты вырос за это время",
+                "Не оставляй...меня...одну",
+                "Спасибо за заботу",
+                "Пока тебе не исполнится 18,\nникаких материалов для взрослых"
+            };
+
+            Console.WriteLine(fraz1[rnd.Next(fraz1.Length)]);
+            Console.Write("Ода, введи число: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Мм, еще, введи второе число: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Как мне сделать, приятно? (-+/*): ");
+            string c = Console.ReadLine();
+            switch (c)
+            {
+                case "+": Console.WriteLine(a + b);
+                    break;
+                case "-": Console.WriteLine(a - b);
+                    break;
+                case "*": Console.WriteLine(a * b);
+                    break;
+                case "/": Console.WriteLine((double)a / (double)b);
+                    break;
+                default: 
+                    break; 
+            }
+            Console.WriteLine("Я буду тебя ждать снова");
+        }
+    }
+}
