2bf3bfac40fcb869510018c897739c41f2306c3f
Тимофей Перминов <goaway0137@outlook.com>
2021-12-31 03:08:59 +0700

    Редакция.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,11 +4,19 @@ namespace ConsoleApp1
 {
     class Program
     {
+        static int num = 0;
         static void Main(string[] args)
         {
+            testFunc(num);
+            testFunc(num);
+        }
+        public static void testFunc(int num)
+        {
+            Program.num++;
+            Console.Write(Program.num);
             int a = 123;
             string b = a.ToString();
             Console.WriteLine(b[2]);
         }
-    }           
+    }
 }
