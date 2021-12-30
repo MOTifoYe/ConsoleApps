14a9866e5428b7bf456d478b9fde0e41e1b3f4f6
Тимофей Перминов <goaway0137@outlook.com>
2021-12-31 03:25:00 +0700

    Редакция.

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -4,23 +4,18 @@ namespace ConsoleApp6
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("ща распечатаю числа, квадрат которых не превышает число");
+            Console.WriteLine("Ща распечатаю числа,\nквадрат которых не превышает число");
             Console.Write("введи число: ");
             int a = Convert.ToInt32(Console.ReadLine());
             int b = 1,c;
             while (true)
             {
                 c = b * b;
-                if (c < a)
-                {
-                    Console.WriteLine(b);
-                }
-                else
-                {
-                    break;
-                }
+                if (c < a) Console.WriteLine(b);
+                else break;
+                
                 b++;
             }
             Console.WriteLine("Числа закончились");
