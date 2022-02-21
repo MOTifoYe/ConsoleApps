aa08ee6d204ad0c488466d91df4ffb74eb0249e4
Тимофей Перминов <goaway0137@outlook.com>
2022-02-22 02:20:26 +0700

    Edit.

--- MathTriangle/Program.cs
+++ MathTriangle/Program.cs
@@ -2,69 +2,55 @@
 
 namespace MathTriangle
 {
-    internal class Program
+    class Program
     {
+
+        const int cellWidth = 1;
+
         static void Main(string[] args)
         {
-            int row = 64;
+            Console.ReadKey();
+            int row = Console.BufferWidth / 2;
             int[,] triangle = new int[row, row];
-            const int cellWidth = 1;
 
-            void FillTriangle()
+            FillTriangle(row, ref triangle);
+            Magic(row, ref triangle);
+
+        }
+        static void FillTriangle(int row, ref int[,] triangle)
+        {
+            for (int i = 0; i < row; i++)
             {
-                for (int i = 0; i < row; i++)
-                {
-                    triangle[i, 0] = 1;
-                    triangle[i, i] = 1;
-                }
-
-                for (int i = 2; i < row; i++)
-                {
-                    for (int j = 1; j <= i; j++)
-                    {
-                        triangle[i, j] =
-                            triangle[i - 1, j - 1] + triangle[i - 1, j];
-                    }
-                }
+                triangle[i, 0] = 1;
+                triangle[i, i] = 1;
             }
 
-            void PrintTriangle()
+            for (int i = 2; i < row; i++)
             {
-                for (int i = 0; i < row; i++)
+                for (int j = 1; j <= i; j++)
                 {
-                    for (int j = 0; j < row; j++)
-                    {
-                        if (triangle[i, j] != 0)
-                            Console.Write($"{triangle[i, j],cellWidth}");
-                    }
-                    Console.WriteLine();
+                    triangle[i, j] =
+                        triangle[i - 1, j - 1] + triangle[i - 1, j];
                 }
             }
+        }
 
-            void Magic()
+        static void Magic(int row, ref int[,] triangle)
+        {
+            int col = cellWidth * row;
+            for (int i = 0; i < row; i++)
             {
-                int col = cellWidth * row;
-                for (int i = 0; i < row; i++)
+                for (int j = 0; j <= i; j++)
                 {
-                    for (int j = 0; j <= i; j++)
-                    {
-                        Console.SetCursorPosition(col, i + 1);
-                        //if (triangle[i, j] != 0) Console.Write($"{triangle[i, j],cellWidth}");
-                        if (triangle[i, j] % 2 != 0) Console.WriteLine("*");
-                        col += cellWidth * 2;
-                    }
-
-                    col = cellWidth * row - cellWidth * (i + 1);
-
-                    Console.WriteLine();
+                    Console.SetCursorPosition(col, i + 1);
+                    if (triangle[i, j] % 2 != 0) Console.WriteLine("*");
+                    col += cellWidth * 2;
                 }
-            }
 
-            Console.ReadLine();
-            FillTriangle();
-            //PrintTriangle();
-            Console.ReadLine();
-            Magic();
+                col = cellWidth * row - cellWidth * (i + 1);
+
+                Console.WriteLine();
+            }
         }
     }
 }
