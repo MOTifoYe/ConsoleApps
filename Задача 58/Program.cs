a0798b36f9e9e93d1775002f63407c75d6739e70
MOTifoYe <goaway0137@outlook.com>
2022-02-23 00:04:19 +0700

    New file project.

---
+++ Задача 58/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Из двухзначных чисел вывести те, сумма цифр которых равна n");
+
+int n = new Random().Next(1, 19);
+
+Console.WriteLine("Число n = {0}", n);
+
+for (int i = 10; i < 100; i++)
+{
+    int a = i % 10;
+    int b = i / 10;
+
+    if (a + b == n) Console.WriteLine(i);
+}
