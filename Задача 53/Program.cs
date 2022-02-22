26b4bce9181a9f5dc5d159e7c16ecfb8e141c06b
Тимофей Перминов <goaway0137@outlook.com>
2022-02-22 23:25:47 +0700

    New file project.

---
+++ Задача 53/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Подсчитать сумму всех чисел \n" +
+    "от 1 до 100 при помощи цикла For.");
+
+int sum = 0;
+for (int i = 0; i <= 100; i++)
+{
+    sum += i;
+}
+
+Console.WriteLine(sum);
