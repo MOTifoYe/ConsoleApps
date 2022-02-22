14143966253b25ddabe0a1343338b4f06c71f04e
Тимофей Перминов <goaway0137@outlook.com>
2022-02-22 23:38:14 +0700

    New file project.

---
+++ Задача 56/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Подсчитать сумму всех чисел, которые делятся на 3 без остатка в интервале от 1 до 100");
+
+int sum = 0;
+for (int i = 0; i <= 100; i++)
+{
+    if (i % 3 == 0) sum += i;
+}
+
+Console.WriteLine(sum);
